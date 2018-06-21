b9f696db0470753e85b6a849b5e7e5310926a282
Bruno Cesar Dias <bcdbruno@gmail.com>
2018-06-21 20:55:23 +0000

    deploying folder

---
+++ DeployingDemo/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The app has Started!!");
+});
